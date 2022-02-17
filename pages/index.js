72b557f8b0d4dd56b3bc75293fec223941b313ef
quentin <quentin.cuoc@outlook.fr>
2022-02-17 06:55:25 +0100

    Fix imported file

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Header from "../Components/Header";
+import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Results from "../components/Results";
 import requests from "../utils/requests";
